7246c7f141fe065dcc2b84d7fdd239ef5f7616ac
furukawa.masaki <10431012+moezakura@users.noreply.github.com>
2021-12-20 10:00:53 +0900

    change config removed same require

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -2,6 +2,6 @@ package config
 
 type Config struct {
 	Targets                    []string `config:"targets,required" yaml:"targets"`
-	FirestoreProjectOnEmulator string   `config:"firestore_project_on_emulator,required" yaml:"firestore_project_on_emulator"`
-	FirestoreEmulatorHost      string   `config:"firestore_emulator_host,required" yaml:"firestore_emulator_host"`
+	FirestoreProjectOnEmulator string   `config:"firestore_project_on_emulator" yaml:"firestore_project_on_emulator"`
+	FirestoreEmulatorHost      string   `config:"firestore_emulator_host" yaml:"firestore_emulator_host"`
 }
