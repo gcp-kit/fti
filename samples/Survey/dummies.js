06e76e81036f19d8d4535d21bdea12b0289a33ec
sigma <seiya.sugimoto.free@gmail.com>
2022-03-04 23:19:39 +0900

    fix js sample data

---
+++ samples/Survey/dummies.js
@@ -0,0 +1,14 @@
+[
+  {
+    ref: "Foo__dummy_0",
+    payload: {
+      defaultOption: "$Option__dummy_0",
+      optionsText: "/${Option__dummy_0}/${Option__dummy_0}/",
+      options: {
+        $Option__dummy_0: true,
+        $Option__dummy_1: true,
+      },
+      answer: "${Option__dummy_1}",
+    },
+  },
+];
