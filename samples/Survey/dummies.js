4fd7582600c6bc1d98dfba45c425f477e48529d8
sigma <seiya.sugimoto.free@gmail.com>
2022-03-30 17:23:40 +0900

    feat []stringとint[]のsampleを追加

--- samples/Survey/dummies.js
+++ samples/Survey/dummies.js
@@ -4,6 +4,8 @@
     payload: {
       defaultOption: "$Option__dummy_0",
       optionsText: "/#{Option__dummy_0}/#{Option__dummy_0}/",
+      optionsArray: ["$Option__dummy_0", "#{Option__dummy_1}(default)"],
+      point: [10, 30],
       options: {
         $Option__dummy_0: true,
         $Option__dummy_1: true,
