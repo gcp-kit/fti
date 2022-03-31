cbecb2520ffad3ef6569d901ad1ebcda5e85792f
sigma <seiya.sugimoto.free@gmail.com>
2022-04-01 04:37:22 +0900

    feat add sample

--- samples/Survey/dummies.js
+++ samples/Survey/dummies.js
@@ -6,6 +6,7 @@
       optionsText: "/#{Option__dummy_0}/#{Option__dummy_0}/",
       optionsArray: ["$Option__dummy_0", "#{Option__dummy_1}(default)"],
       point: [10, 30],
+      postText: ["10", "30"],
       options: {
         $Option__dummy_0: true,
         $Option__dummy_1: true,
